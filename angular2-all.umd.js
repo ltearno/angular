42199081ce18af72ed0d280b20e695556ee89b4c
Arnaud Tournier <ltearno@gmail.com>
2016-07-17 09:46:50 +0200

    fix angular 2 gwt bundle

--- angular2-all.umd.js
+++ angular2-all.umd.js
@@ -1,12 +1,12 @@
-exports.core = require('angular2/core');
-exports.common = require('angular2/common');
-exports.compiler = require('angular2/compiler');
+exports.core = require('@angular/core');
+exports.common = require('@angular/common');
+exports.compiler = require('@angular/compiler');
 exports.platform = {
-  browser: require('angular2/platform/browser'),
-  common_dom: require('angular2/platform/common_dom')
+  browser: require('@angular/platform/browser'),
+  common_dom: require('@angular/platform/common_dom')
 };
-exports.http = require('angular2/http');
+exports.http = require('@angular/http');
 exports.router = require('@angular/router');
-exports.router_link_dsl = require('angular2/router/router_link_dsl.js');
-exports.instrumentation = require('angular2/instrumentation');
-exports.upgrade = require('angular2/upgrade');
+exports.router_link_dsl = require('@angular/router/router_link_dsl.js');
+exports.instrumentation = require('@angular/instrumentation');
+exports.upgrade = require('@angular/upgrade');
