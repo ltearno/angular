b6daa2d1697daf128f8b59313e5df0c2e40f174d
Arnaud Tournier <ltearno@gmail.com>
2016-07-17 22:13:47 +0200

    fixes

--- docker/buildAngularGwt.sh
+++ docker/buildAngularGwt.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 cd /
-
 git clone https://github.com/ltearno/angular.git --depth=1
 
+cd angular
 npm install
-
-./build
+./build.sh
 
 npm install compression-webpack-plugin
-
 cp -R dist/all/@angular node_modules/
-
 $(npm bin)/webpack
