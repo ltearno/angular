5174cda6c2dbfa6e343bc2e179cd175adb4f2f0a
Arnaud Tournier <ltearno@gmail.com>
2016-08-21 13:48:38 +0200

    clean

--- angular2-all.umd.js
+++ angular2-all.umd.js
@@ -4,17 +4,10 @@ exports.router = require('@angular/router');
 exports.forms = require('@angular/forms');
 exports.platformBrowser = require('@angular/platform-browser');
 exports.http = require('@angular/http');
-
 exports.rxjs = require('rxjs');
 
-//var Observable = exports.rxjs.Observable;
-//require('rxjs/add/operator/map');
-
-//exports.Observable = Observable;
-
-//exports.rxjs = require('rxjs');
-	//exports.common = require('@angular/common');
-	//exports.compiler = require('@angular/compiler');
-	//exports.router_link_dsl = require('@angular/router/router_link_dsl.js');
-	//exports.instrumentation = require('@angular/instrumentation');
-	//exports.upgrade = require('@angular/upgrade');
+//exports.common = require('@angular/common');
+//exports.compiler = require('@angular/compiler');
+//exports.router_link_dsl = require('@angular/router/router_link_dsl.js');
+//exports.instrumentation = require('@angular/instrumentation');
+//exports.upgrade = require('@angular/upgrade');
